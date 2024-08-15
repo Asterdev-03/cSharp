d54f9022ca82b9a8f11a9e0bc9ffec95927fee64
aswin <aswinkarat@gmail.com>
2024-08-15 15:03:31 +0530

    C# Basics: Variable definitions and Read & Write on Console

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Variable definition */
+
+            var hello = "hello";
+            string str = "banana";
+            int age;
+            age = 5;
+            bool isOk = true;
+            char letter = 'a';
+            double fraction = 1.25;
+            decimal precision = 23.445m;
+
+
+            /* -------------------------------------------------------------------------------------------- */
+
+            /* Read and Write on Console */
+
+            Console.WriteLine("Write something: ");
+            // var input = Console.ReadLine();
+            var input = "hi";
+            Console.Write(input);
+            Console.WriteLine($" {hello} {str} {age} {isOk} {letter} {fraction} {precision}");
+
+            /* -------------------------------------------------------------------------------------------- */
+        }
+    }
+}
